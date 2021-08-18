04e837c755b3918b7d3c2a17ed6a141b3456f8df
RikRogaar <rikrogaar@gmail.com>
2021-08-18 22:07:17 +0200

    Disabled success image

--- src/components/signuppage/FormSuccess.js
+++ src/components/signuppage/FormSuccess.js
@@ -4,7 +4,7 @@ const FormSuccess = () => {
     return (
         <div className='form-content-right'>
             <div className="form-success">We have recieved your request!</div>
-            <img src="../images/img-3.svg" alt="success-image"></img>
+             {/* <img src='/images/img-18.svg' alt='success' /> */}
         </div>
     )
 }
